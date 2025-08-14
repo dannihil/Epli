9ffec39d4a07dde7e05d70943fb2546182ba4f9a
dannihil <dannihil@gmail.com>
2025-08-14 08:56:48 +0000

    -

--- src/pages/Cto.jsx
+++ src/pages/Cto.jsx
@@ -87,10 +87,6 @@ function Cto() {
                 <p className="category-text">Mac Pro</p>
               </div>
             </div>
-            <h2 style={{ marginTop: "30px" }}>
-              ATH! þessar síður eru í vinnslu, verð gætu verið röng,
-              vinsamlegast berið saman við excel skjalið.
-            </h2>
           </div>
         </div>
       </SignedIn>
